01a80aa7811d85db25722ced2a4c2fb316c6f161
BryanPineda21 <109675212+BryanPineda21@users.noreply.github.com>
2023-04-28 18:00:59 -0400

    3 files modified

--- counter.js
+++ counter.js
@@ -1,1 +1,32 @@
 // TODO: Add counter code here.
+
+
+// A display that shows the user what the count is at. The count should be initialized at 0.
+
+const countSpan = document.getElementById('count');
+const incrementButton = document.querySelector('#increment');
+const decrementButton = document.querySelector('#decrement');
+
+
+countSpan.innerText = 0;
+
+
+
+
+
+
+// A decrement button that subtracts 1 from the current count and updates the display.
+
+
+decrement.addEventListener('click', () => {
+  console.log('decrement button clicked');
+});
+
+// An increment button that adds 1 to the current count and updates the display.
+
+
+
+
+increment.addEventListener('click', () => {
+  console.log('increment button clicked');
+});
